cab22f68c7d898d6978c0684b2fb4859a8a6ce77
Overlord2410 <61825598+Overlord2410@users.noreply.github.com>
2020-08-14 12:28:11 +0200

    Update main.c

--- Esercizio 3 luglio 2019/main.c
+++ Esercizio 3 luglio 2019/main.c
@@ -26,6 +26,30 @@ int is_simmetric(int mat[N][N]){
     return flag;
 }
 
+
+//per "scorrere" la matrice in modo ricorsivo si usano gli indici i,j come parametri
+//il primo caso base, quello in cui si controlla se i == N serve per capire se si è
+//arrivati alla fine della matrice. Arrivati alla fine senza errori pertanto ritorniamo
+//il valore di verità.
+//il secondo caso base è il vero e proprio controllo di simmetricità della matrice
+//che ritorna il valore "falso" se la matrice non è simmetrica
+
+int is_simmetric_recursive(int mat[N][N], int i, int j){
+    if (i == N){
+        return 1;//True
+    }else if (mat[i][j] != mat[j][i]){
+        return 0;//False
+    }else{
+        
+        if (j == N){
+            return is_simmetric_recursive(mat, i + 1, 0);//a fine riga (j == N) passo alla riga dopo (i + 1) primo elemento (0)
+        }else{
+            return is_simmetric_recursive(mat, i, j + 1);// in una qualsiasi posizione, incremento j
+        }
+        
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
     return 0;
